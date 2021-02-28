d39903c76ac8a9a95e3c59412b10ea02eca3913d
maxdanko2000 <maxdanko12345@yandex.ru>
2021-02-28 22:23:42 +0300

    init: start and complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let run;
+  while (str.length !== 0) {
+      for (brackets of bracketsConfig) {
+          let temp = brackets.join("");
+          str = str.replace(temp, "");
+      }
+
+      if (str === run) {
+          break;
+      }
+      run = str;
+  }
+  return str.length === 0;
+};
